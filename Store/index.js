3eeccc3ade8018419fe478ebcd5d9497b2042055
KingOfKings01 <aasifkhan9605@gmail.com>
2024-10-23 12:24:24 +0530

    Add toggle on counter and handle counter and toggle state both at a time.

--- Store/index.js
+++ Store/index.js
@@ -1,19 +1,17 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter : 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
   switch (action.type) {
     case "increment":
-      return { counter: state.counter + 1 };
+      return {...state,  counter: state.counter + 1 };
     case "decrement":
-      return { counter: state.counter - 1 };
-    case "INCREMENTBY2":
-      return { counter: state.counter + 2 };
-    case "DECREMENTBY2":
-      return { counter: state.counter - 2 };
-    case "INCREMENTBY5":
-      return { counter: state.counter + 5 };
-    case "DECREMENTBY5":
-      return { counter: state.counter - 5 };
+      return {...state,  counter: state.counter - 1 };
+    case "IncrementByAmount":
+      return {...state, counter: state.counter + action.amount };
+    case "showCounter":
+      return { ...state, showCounter: !state.showCounter}
     default:
       return state;
   }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux"
 
 
 export default function Counter() {
 
+    const [amount, setAmount] = useState(15);
+
     const counter = useSelector(state => state.counter) // The callback function will get called by react-redux
+    const showCounter = useSelector(state => state.showCounter)
     const dispatch = useDispatch()
 
     const incrementHandler = () => {
@@ -14,28 +18,32 @@ export default function Counter() {
         dispatch({ type: "decrement" })
     }
 
-    const incrementBy5Handler = () => {
-        dispatch({ type: "INCREMENTBY5" })
+    const incrementByAmountHandler = () => {
+        dispatch({ type: "IncrementByAmount", amount: amount })
     }
-    
-    const decrementBy5Handler = () => {
-        dispatch({ type: "DECREMENTBY5" })
+
+    const handleToggle = () => {
+        dispatch({ type: "showCounter" })
     }
+
 
     return (
         <main>
             <h1>Redux Counter</h1>
-            <div>{counter}</div>
+            {showCounter ?
+                <div>{counter}</div>
+                :
+                <p></p>
+            }
             <br />
             <div>
                 <button onClick={incrementHandler}>Increment 1</button>
                 <button onClick={decrementHandler}>Decrement 1</button>
             </div>
             <div>
-                <button onClick={incrementBy5Handler}>Increment 5</button>
-                <button onClick={decrementBy5Handler}>Decrement 5</button>
+                <button onClick={incrementByAmountHandler}>Increment {amount}</button>
             </div>
-            {/* <button>Toggle Counter</button> */}
+            <button onClick={handleToggle}>Toggle Counter</button>
 
         </main>
     )
